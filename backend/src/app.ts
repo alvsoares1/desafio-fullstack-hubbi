15fc83ff3b6f90656786b350ec94285d6070215e
alvsoares1 <alvaroass2110@gmail.com>
2025-01-28 16:40:32 -0300

    fix: Correção da URL do frontend na corsOptions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: 'http://localhost:3000', 
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 
 };
